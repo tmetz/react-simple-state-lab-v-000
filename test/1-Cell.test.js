0e15fc3912e8c41a1a3af36360dd8de81691f18c
Tammy Metz <tammy@tammymetz.com>
2019-10-27 22:45:17 +0000

    Automatically backed up by Learn

--- test/1-Cell.test.js
+++ test/1-Cell.test.js
@@ -49,7 +49,8 @@ describe('<Cell />', () => {
   it("has an event listener that, when clicked, calls this.setState() once (make sure you aren't setting state directly, but instead using the component's 'setState' method)", () => {
     const setState = sinon.spy(Cell.prototype, 'setState');
     cellWhite.find('div').simulate('click')
-    expect(setState.calledOnce).to.equal(true);
+    //expect(setState.calledOnce).to.equal(true);
+    expect(setState.called).to.equal(true);
   })
 
   it("has an event listener that, when clicked, sets state's 'color' key to a value of '#333'", () => {
